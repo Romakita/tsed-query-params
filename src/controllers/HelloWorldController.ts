9ad30d7334d95e1ee86c5dafc6279b020985f774
Romain Lenzotti <rom.lenzotti@gmail.com>
2021-08-29 11:26:40 +0200

    chore: add snake

--- src/controllers/HelloWorldController.ts
+++ src/controllers/HelloWorldController.ts
@@ -2,8 +2,13 @@ import {Controller, Get, QueryParams} from "@tsed/common";
 
 @Controller("/hello-world")
 export class HelloWorldController {
-  @Get("/")
-  get(@QueryParams("camelCase") test: string) {
+  @Get("/camel")
+  get1(@QueryParams("camelCase") test: string) {
+    return `hello ${test}`;
+  }
+
+  @Get("/snake")
+  get2(@QueryParams("snake_case") test: string) {
     return `hello ${test}`;
   }
 }
